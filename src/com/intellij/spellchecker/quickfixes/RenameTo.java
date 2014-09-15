c7cb5c1f74f6a7164506b809592b78a6f610a8f1
VISTALL <vistall.valeriy@gmail.com>
2014-09-15 19:01:23 +0400

    compilation

--- src/com/intellij/spellchecker/quickfixes/RenameTo.java
+++ src/com/intellij/spellchecker/quickfixes/RenameTo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,10 +15,22 @@
  */
 package com.intellij.spellchecker.quickfixes;
 
+import java.awt.Component;
+import java.awt.KeyboardFocusManager;
+
+import javax.swing.SwingUtilities;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.ide.DataManager;
 import com.intellij.injected.editor.EditorWindow;
-import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.Anchor;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.impl.SimpleDataContext;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Editor;
@@ -34,109 +46,137 @@ import com.intellij.refactoring.rename.NameSuggestionProvider;
 import com.intellij.refactoring.rename.RenameHandlerRegistry;
 import com.intellij.spellchecker.util.SpellCheckerBundle;
 import com.intellij.util.containers.HashMap;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import javax.swing.*;
-import java.awt.*;
 
 
-public class RenameTo extends ShowSuggestions implements SpellCheckerQuickFix {
+public class RenameTo extends ShowSuggestions implements SpellCheckerQuickFix
+{
 
-  public RenameTo(String wordWithTypo) {
-    super(wordWithTypo);
-  }
+	public RenameTo(String wordWithTypo)
+	{
+		super(wordWithTypo);
+	}
 
-  @NotNull
-  public String getName() {
-    return SpellCheckerBundle.message("rename.to");
-  }
+	@Override
+	@NotNull
+	public String getName()
+	{
+		return SpellCheckerBundle.message("rename.to");
+	}
 
-  @NotNull
-  public String getFamilyName() {
-    return SpellCheckerBundle.message("rename.to");
-  }
-
-
-  @Nullable
-  private static DictionarySuggestionProvider findProvider() {
-    Object[] extensions = Extensions.getExtensions(NameSuggestionProvider.EP_NAME);
-
-    for (Object extension : extensions) {
-      if (extension instanceof DictionarySuggestionProvider) {
-        return (DictionarySuggestionProvider)extension;
-      }
-    }
-    return null;
-  }
+	@Override
+	@NotNull
+	public String getFamilyName()
+	{
+		return SpellCheckerBundle.message("rename.to");
+	}
 
 
-  @NotNull
-  public Anchor getPopupActionAnchor() {
-    return Anchor.FIRST;
-  }
+	@Nullable
+	private static DictionarySuggestionProvider findProvider()
+	{
+		Object[] extensions = Extensions.getExtensions(NameSuggestionProvider.EP_NAME);
 
-  @SuppressWarnings({"SSBasedInspection"})
-  public void applyFix(@NotNull final Project project, @NotNull final ProblemDescriptor descriptor) {
-    Runnable fix = new Runnable() {
-      public void run() {
-        DictionarySuggestionProvider provider = findProvider();
-        if (provider != null) {
-          provider.setActive(true);
-        }
+		for(Object extension : extensions)
+		{
+			if(extension instanceof DictionarySuggestionProvider)
+			{
+				return (DictionarySuggestionProvider) extension;
+			}
+		}
+		return null;
+	}
 
-        Editor editor = getEditorFromFocus();
-        HashMap<String, Object> map = new HashMap<String, Object>();
-        PsiElement psiElement = descriptor.getPsiElement();
-        if (psiElement == null) return;
-        PsiFile containingFile = psiElement.getContainingFile();
-        if (editor == null) {
-          editor = InjectedLanguageUtil.openEditorFor(containingFile, project);
-        }
 
-        if (editor == null) return;
+	@Override
+	@NotNull
+	public Anchor getPopupActionAnchor()
+	{
+		return Anchor.FIRST;
+	}
 
-        if (editor instanceof EditorWindow) {
-          map.put(PlatformDataKeys.EDITOR.getName(), editor);
-          map.put(LangDataKeys.PSI_ELEMENT.getName(), psiElement);
-        } else if (ApplicationManager.getApplication().isUnitTestMode()) { // TextEditorComponent / FiledEditorManagerImpl give away the data in real life
-          map.put(
-            LangDataKeys.PSI_ELEMENT.getName(), 
-            new TextEditorPsiDataProvider().getData(
-              LangDataKeys.PSI_ELEMENT.getName(),
-              editor, 
-              containingFile.getVirtualFile()
-            )
-          );
-        }
+	@Override
+	@SuppressWarnings({"SSBasedInspection"})
+	public void applyFix(@NotNull final Project project, @NotNull final ProblemDescriptor descriptor)
+	{
+		Runnable fix = new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				DictionarySuggestionProvider provider = findProvider();
+				if(provider != null)
+				{
+					provider.setActive(true);
+				}
 
-        final Boolean selectAll = editor.getUserData(RenameHandlerRegistry.SELECT_ALL);
-        try {
-          editor.putUserData(RenameHandlerRegistry.SELECT_ALL, true);
-          DataContext dataContext = SimpleDataContext.getSimpleContext(map, DataManager.getInstance().getDataContext(editor.getComponent()));
-          AnAction action = new RenameElementAction();
-          AnActionEvent event = new AnActionEvent(null, dataContext, "", action.getTemplatePresentation(), ActionManager.getInstance(), 0);
-          action.actionPerformed(event);
-          if (provider != null) {
-            provider.setActive(false);
-          }
-        }
-        finally {
-          editor.putUserData(RenameHandlerRegistry.SELECT_ALL, selectAll);
-        }
-      }
-    };
-    
-    if (ApplicationManager.getApplication().isUnitTestMode()) fix.run();
-    else SwingUtilities.invokeLater(fix); // TODO [shkate] this is hard to test!
-  }
+				Editor editor = getEditorFromFocus();
+				HashMap<String, Object> map = new HashMap<String, Object>();
+				PsiElement psiElement = descriptor.getPsiElement();
+				if(psiElement == null)
+				{
+					return;
+				}
+				PsiFile containingFile = psiElement.getContainingFile();
+				if(editor == null)
+				{
+					editor = InjectedLanguageUtil.openEditorFor(containingFile, project);
+				}
 
-  @Nullable
-  private static Editor getEditorFromFocus() {
-    final Component c = KeyboardFocusManager.getCurrentKeyboardFocusManager().getFocusOwner();
-    if (c instanceof EditorComponentImpl) {
-      return ((EditorComponentImpl)c).getEditor();
-    }
-    return null;
-  }
+				if(editor == null)
+				{
+					return;
+				}
+
+				if(editor instanceof EditorWindow)
+				{
+					map.put(CommonDataKeys.EDITOR.getName(), editor);
+					map.put(CommonDataKeys.PSI_ELEMENT.getName(), psiElement);
+				}
+				else if(ApplicationManager.getApplication().isUnitTestMode())
+				{ // TextEditorComponent / FiledEditorManagerImpl give away the data in real life
+					map.put(CommonDataKeys.PSI_ELEMENT.getName(), new TextEditorPsiDataProvider().getData(CommonDataKeys.PSI_ELEMENT.getName(),
+							editor, editor.getCaretModel().getCurrentCaret()));
+				}
+
+				final Boolean selectAll = editor.getUserData(RenameHandlerRegistry.SELECT_ALL);
+				try
+				{
+					editor.putUserData(RenameHandlerRegistry.SELECT_ALL, true);
+					DataContext dataContext = SimpleDataContext.getSimpleContext(map, DataManager.getInstance().getDataContext(editor.getComponent
+							()));
+					AnAction action = new RenameElementAction();
+					AnActionEvent event = new AnActionEvent(null, dataContext, "", action.getTemplatePresentation(), ActionManager.getInstance(), 0);
+					action.actionPerformed(event);
+					if(provider != null)
+					{
+						provider.setActive(false);
+					}
+				}
+				finally
+				{
+					editor.putUserData(RenameHandlerRegistry.SELECT_ALL, selectAll);
+				}
+			}
+		};
+
+		if(ApplicationManager.getApplication().isUnitTestMode())
+		{
+			fix.run();
+		}
+		else
+		{
+			SwingUtilities.invokeLater(fix); // TODO [shkate] this is hard to test!
+		}
+	}
+
+	@Nullable
+	private static Editor getEditorFromFocus()
+	{
+		final Component c = KeyboardFocusManager.getCurrentKeyboardFocusManager().getFocusOwner();
+		if(c instanceof EditorComponentImpl)
+		{
+			return ((EditorComponentImpl) c).getEditor();
+		}
+		return null;
+	}
 }
